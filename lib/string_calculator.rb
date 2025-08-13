123f008e9b849e21751ee4ff0d7ffe6be2aebb27
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-08-13 12:30:13 +0530

    feat(GREEN): support any count of comma-separated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,12 +3,6 @@
 class StringCalculator
   def add(numbers)
     s = numbers.to_s
-
-    if s.include?(',')
-      a, b = s.split(',', 2)
-      return a.to_i + b.to_i
-    end
-
-    s.to_i
+    s.split(",").map(&:to_i).sum
   end
 end
