32f6c892b55128bb788c4c979b2c144920a9d090
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-08-13 12:27:08 +0530

    test(RED): two comma-separated numbers ('1,2' => 3)

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,5 +12,10 @@ RSpec.describe StringCalculator do
       expect(calc.add("1")).to eq(1)
       expect(calc.add("7")).to eq(7)
     end
+
+    it 'sums two comma-separated numbers: "1,2" => 3' do
+      expect(calc.add("1,2")).to eq(3)
+      expect(calc.add("10,5")).to eq(15)
+    end
   end
 end
