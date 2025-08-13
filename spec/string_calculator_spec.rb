8d0ad8648b92ced51699c407827e41719a82622b
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-08-13 12:14:07 +0530

    test(RED): single number returns itself (e.g. '1' => 1)

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string: "" => 0' do
       expect(calc.add("")).to eq(0)
     end
+
+    it "returns the number for a single numeric string" do
+      expect(calc.add("1")).to eq(1)
+      expect(calc.add("7")).to eq(7)
+    end
   end
 end
