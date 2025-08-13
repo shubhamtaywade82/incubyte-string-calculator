4ec396917acea20be257d923dbcd58ccfcb9b19f
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-08-13 12:36:00 +0530

    feat(GREEN): parse //header and use custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,13 @@ class StringCalculator
   def add(numbers)
     s = numbers.to_s
     delimiters = [",", "\n"]
+
+    if s.start_with?("//")
+      header, rest = s.split("\n", 2)
+      delimiters << header[2..]       # single custom delimiter after //
+      s = rest.to_s                   # rest contains the actual numbers
+    end
+
     tokens = s.split(Regexp.union(delimiters)).reject(&:empty?)
     tokens.map(&:to_i).sum
   end
