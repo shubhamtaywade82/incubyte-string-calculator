9d5f25b6630150fb7ed2b5b3517337baf33f1fe5
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-08-13 12:28:29 +0530

    feat(GREEN): handle two comma-separated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 class StringCalculator
   def add(numbers)
-    numbers.to_s.to_i
+    s = numbers.to_s
+
+    if s.include?(',')
+      a, b = s.split(',', 2)
+      return a.to_i + b.to_i
+    end
+
+    s.to_i
   end
 end
